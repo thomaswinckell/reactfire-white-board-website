b0e9cf168135ac38cd2a96ae6988632a449e4bdf
brillard.t <brillard.t@sfeir.lu>
2016-04-05 10:48:00 +0200

    Création BoardManager.jsx
    Tests pour comprendre dépendances

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component }     from 'react';
 
+import BoardManager             from 'core/BoardManager';
+
 export default class App extends Component {
 
     render() {
         return (
             <div>
-                Hello world !
+                <BoardManager />
             </div>
         );
     }
---
+++ src/core/BoardManager.jsx
@@ -0,0 +1,28 @@
+import _                        from 'lodash';
+import $                        from 'jquery';
+import React,
+       { Component, PropTypes } from 'react';
+import ReactDOM                 from 'react-dom';
+
+
+export default class BoardManager extends Component  {
+
+    constructor() {
+        this.state = {};
+    }
+
+
+    
+
+    render(){
+
+        return(
+            <p>
+                Lol ça marche :P
+            </p>
+        )
+
+    }
+
+
+}
