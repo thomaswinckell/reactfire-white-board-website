f0e9eeb88b4ef6e9210fefa87e1b3b263744b50c
brillard.t <brillard.t@sfeir.lu>
2016-04-05 11:37:53 +0200

    Creation SearchBoard.jsx
    Creation of the SearchBoard classs
    TODO next month

---
+++ src/core/SearchBoard.jsx
@@ -0,0 +1,24 @@
+import _                        from 'lodash';
+import $                        from 'jquery';
+import React,
+       { Component, PropTypes } from 'react';
+import ReactDOM                 from 'react-dom';
+
+
+export default class SearchBoard extends Component  {
+
+    constructor( props ) {
+        super( props );
+        this.state = {};
+    }
+
+    render(){
+
+        return(
+          <h3> TODO NEXT MONTH </h3>
+        )
+
+    }
+
+
+}
