154d40e4073a4db3be077b97d9a4ae3e54001a84
Thomas Winckell <winckell.t@sfeir.lu>
2016-04-13 18:12:52 +0200

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 git checkout master
+npm install
 npm run build
 git checkout gh-pages
 git rm -rf .
 cp -R build/* .
+rm -rf node_modules/
+rm -rf build/
 git add ./css
 git add ./fonts
 git add ./js
 git add index.html
 git commit -m "Publish"
-git push origin/gh-pages
+git push origin gh-pages
 git rm -rf .
-rm -rf node_modules/
-rm -rf build/
