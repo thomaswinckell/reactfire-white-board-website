db2e36968d7958ac987182944d31fd2e921f969f
Thomas BRILLARD <brillard.t@sfeir.lu>
2016-04-14 10:28:31 +0200

    Intl provider now in app.jsx

--- src/app.js
+++ src/app.js
@@ -1,51 +1,15 @@
 import 'index.html';
-import 'theme/main.scss';
+//import 'theme/main.scss';
 
 import React                    from 'react';
 import ReactDOM                 from 'react-dom';
 import App                      from 'core/App';
 
-import {addLocaleData,
-        IntlProvider}           from 'react-intl';
-
-import en                       from 'react-intl/locale-data/en';
-import fr                       from 'react-intl/locale-data/fr';
-import frMessages               from 'i18n/locales/fr.json';
-import enMessages               from 'i18n/locales/en.json';
 import injectTapEventPlugin     from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
-addLocaleData([...en, ...fr]);
-
-// we need to make sure we transform the given locale to the right format first
-// so we can access the right locale in our dictionaries for example: pt-br should be transformed to pt-BR
-function formatLocale(lang) {
-  lang = lang.split('-');
-  return lang[0];
-}
-
-
-var localeNav = formatLocale(navigator.language);
-//var localeNav = formatLocale('fr-CA');
-
-function getLocalMessage(locale){
-    console.log(locale.localeNav);
-    switch (locale.localeNav) {
-        case 'fr':
-            return frMessages;
-        case 'en':
-            return enMessages;
-        default: return enMessages;
-
-    }
-}
 
 ReactDOM.render(
-    <IntlProvider
-        locale={localeNav}
-        messages={getLocalMessage({localeNav})}
-    >
-        <App/>
-    </IntlProvider>,
+        <App/>,
     document.getElementById( 'app-container' )
 );
--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,6 +1,13 @@
 import React, { Component }     from 'react';
 import { FluxComponent }        from 'airflux';
 
+import en                       from 'react-intl/locale-data/en';
+import fr                       from 'react-intl/locale-data/fr';
+import frMessages               from 'i18n/locales/fr.json';
+import enMessages               from 'i18n/locales/en.json';
+import {addLocaleData,
+        IntlProvider}           from 'react-intl';
+
 import BoardManager             from 'core/BoardManager';
 import AuthStore                from 'core/AuthStore';
 import BoardManagerStore        from 'core/BoardManagerStore';
@@ -8,6 +15,29 @@ import HeaderApp                from 'core/HeaderApp';
 import AppLoader                from 'core/AppLoader';
 import AccessDenied             from 'core/AccessDenied';
 
+
+addLocaleData([...en, ...fr]);
+
+// we need to make sure we transform the given locale to the right format first
+// so we can access the right locale in our dictionaries for example: pt-br should be transformed to pt-BR
+function formatLocale(lang) {
+  lang = lang.split('-');
+  return lang[0];
+}
+
+
+//var localeNav = formatLocale('fr-CA');
+
+function getLocalMessage(locale){
+    switch (locale.localeNav) {
+        case 'fr':
+            return frMessages;
+        case 'en':
+            return enMessages;
+        default: return enMessages;
+
+    }
+}
 
 @FluxComponent
 export default class App extends Component {
@@ -25,6 +55,7 @@ export default class App extends Component {
 
         const { currentUser } = this.state.authStore;
         const { boards } = this.state.boardManagerStore;
+        let localeNav = formatLocale(navigator.language);
 
         //Render AppLoader screen until data are loaded or user is not logged in
         if ( !currentUser ) {
@@ -36,10 +67,12 @@ export default class App extends Component {
         }
 
         return (
-            <div>
-                <HeaderApp />
-                <BoardManager boards = {boards} />
-            </div>
+            <IntlProvider locale={localeNav} messages={getLocalMessage({localeNav})}>
+                <div>
+                    <HeaderApp />
+                    <BoardManager boards = {boards} />
+                </div>
+            </IntlProvider>
         );
     }
 }
