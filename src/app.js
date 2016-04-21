35ae44276a374f7096f5b72f208769e37e7f9470
Thomas BRILLARD <brillard.t@sfeir.lu>
2016-04-21 16:47:46 +0200

    clean url available
    works with firebase hosting with "rewrites": [
        {
          "source":
    "**",
          "destination": "/index.html"
        }
      ]

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ import ReactDOM                 from 'react-dom';
 import App                      from 'core/App';
 import About                    from 'core/About';
 import WhiteboardView           from 'board/WhiteboardView';
+import Error404                 from 'error/Error404';
 
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 
 
 import injectTapEventPlugin     from 'react-tap-event-plugin';
@@ -15,10 +16,11 @@ import injectTapEventPlugin     from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 ReactDOM.render((
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
        <Route path="/" component={App}>
            <Route path="/boards/:boardKey" component={WhiteboardView}/>
            <Route path="/about" component={About}/>
+           <Route path="*" component={Error404}/>
        </Route>
      </Router>
     ), document.getElementById('app-container')
